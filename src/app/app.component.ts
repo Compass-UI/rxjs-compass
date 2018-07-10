8613e8723a386bb77e2539fd30c7b02cf85c5146
David Shams <david.shams@gmail.com>
2018-07-09 22:57:49 -0500

    first complete Reactive Program using RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+// import { from } from 'rxjs/operators';
+import { Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit(){
+    this.source.subscribe(new MyObserver())
+
+  }
+
+  numbers = [1,2,3,60]
+  source = from(this.numbers)
+}
+
+class MyObserver {
+  next(value) {
+    console.log(`value: ${value}`)
+  }
+
+  error(e){
+    console.log(`Error occured ${e}`)
+  }
+
+  complete(){
+    console.log('complete')
+  }
 }
